fe5ce4c321ce9a2dcebb4159bbb300c2d51a582e
Navneet Verma <navneev@amazon.com>
2022-12-22 16:37:37 -0800

    Added the changes to test the geoshape aggregations
    Signed-off-by: Navneet Verma <navneev@amazon.com>

--- src/plugins/data/common/search/aggs/buckets/geo_hash.ts
+++ src/plugins/data/common/search/aggs/buckets/geo_hash.ts
@@ -64,7 +64,7 @@ export const getGeoHashBucketAgg = () =>
       {
         name: 'field',
         type: 'field',
-        filterFieldTypes: OSD_FIELD_TYPES.GEO_POINT,
+        filterFieldTypes: [OSD_FIELD_TYPES.GEO_POINT, OSD_FIELD_TYPES.GEO_SHAPE],
       },
       {
         name: 'autoPrecision',
